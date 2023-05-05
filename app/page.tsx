74bc760ca0dfe7f5e78b8c82769fda1155165004
carsonkrueger <crkruege@gmail.com>
2023-05-05 09:46:12 -0600

    working on site

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeaderIcon from "./(components)/HeaderIcon";
 import Button from "./(components)/Button";
+import Project from "./(components)/Project";
 
 export default function Home() {
   return (
@@ -34,21 +35,42 @@ export default function Home() {
         </div>
       </header>
 
-      <div className="max-w-screen-md border-white flex flex-col pt-[8rem] font-mono text-white text-lg space-y-3 md:[&>*]:mr-5">
-        <div>Hi my names is...</div>
-        <div className="flex text-teal-500 text-4xl sm:text-6xl">
-          carson_
-          <div className="text-orange-600">kreuger</div>
+      <div className="flex flex-col lg:justify-around lg:flex-row min-w-full my-auto text-white font-mono text-lg space-y-24">
+        <div className="flex flex-col space-y-3 my-auto lg:max-w-screen-md">
+          <div>Hi my names is...</div>
+          <div className="flex text-teal-500 text-4xl sm:text-6xl">
+            Carso
+            <div className="text-white">n_</div>
+            <div className="text-orange-600">Kreuger</div>
+          </div>
+          <div className="">
+            I&apos;m an aspiring front-end developer with a lot of passion for
+            Computer Science! I&apos;m currently enrolled in the CS program at
+            Utah Valley University, expected to graduate Spring of 2024.
+          </div>
+          <Button
+            className="flex items-center justify-center h-10 w-24 sm:mt-7 border border-orange-600 text-orange-600 rounded-md text-[1rem]"
+            text="Contact"
+          />
         </div>
-        <div className="">
-          I'm an aspiring front-end developer with a lot of passion for Computer
-          Science! I'm currently enrolled in the CS program at Utah Valley
-          University, expected to graduate Spring of 2024.
+
+        <div className="my-auto max-w-screen-sm lg:max-w-screen-xs lg:pt-28 space-y-5">
+          <div className="flex text-4xl ">
+            <div className="text-orange-600">Pro</div>
+            je
+            <div className="text-teal-500">ct</div>s
+          </div>
+          <Project
+            name="Repr"
+            body="An Android mobile app on the Google Play Store used for recording workout weights and reps. Built using React Native & Expo."
+            href="https://play.google.com/store/apps/details?id=com.crkruege00.REPR"
+          />
+          <Project
+            name="Snake Shake"
+            body="A snake game built using React JS, and vite, hosted using Vercel."
+            href="https://snake-shake.vercel.app/"
+          />
         </div>
-        <Button
-          className="flex items-center justify-center h-10 w-24 sm:mt-7 border border-orange-600 text-orange-600 rounded-md text-[1rem]"
-          text="Contact"
-        />
       </div>
     </div>
   );
